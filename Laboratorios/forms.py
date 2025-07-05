678754119d289b5a104b8fe2a985712a5075d13d
valenmaza <vmaza.24@est.ucab.edu.ve>
2025-07-04 20:05:20 -0400

    Delete Laboratorios directory

--- Laboratorios/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class NuevoLaboratorio(forms.Form):
-    nombre = forms.CharField(label="ingrese nombre del laboratorio", max_length=200)
-    direccion = forms.CharField(label="ingrese la direccion del laboratorio", max_length=200)
