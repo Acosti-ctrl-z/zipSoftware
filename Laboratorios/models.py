7ab5b2ede3cc6291b1f2d0b5ae50d3418abfc741
Jesús Acosta <160048127+Acosti-ctrl-z@users.noreply.github.com>
2025-07-06 02:39:00 -0400

    Delete Laboratorios directory

--- Laboratorios/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Laboratorios(models.Model):
-    nombre = models.CharField(max_length=100)
-    direccion = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nombre
